edf219ac818784eb16489e563b0032e6da2deb8c
Aarushi <aarushigoyal121@gmail.com>
2024-12-09 17:10:16 +0530

    fix deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  reactStrictMode: true,
   images: { unoptimized: true },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/intro' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/intro/' : '',
+  basePath: '/intro', // process.env.NODE_ENV === 'production' ? '/intro' : '',
+  assetPrefix: '/intro/', //process.env.NODE_ENV === 'production' ? '/intro/' : '',
   trailingSlash: true,
 };
 
